69d632a70c547ed3275fbbec269e78d6b24f26f3
Guillaume-BR <114143077+Guillaume-BR@users.noreply.github.com>
2025-11-04 15:58:32 +0100

    preprocessor

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import pandas as pd
 
-# 1. load the trained model
+# ==============================
+# 1. Chargement du modèle et du préprocesseur
+# ==============================
 with open('model/xgb_best_model.pkl', 'rb') as f:
     model = joblib.load(f)
+
+with open('model/preprocessor.pkl', 'rb') as f:
+    preprocessor = joblib.load(f)
 
 # 2. create FastAPI app
 app = FastAPI(title = "Prédiction de la durée d'hospitalisation API")
@@ -20,31 +26,35 @@ class PatientData(BaseModel):
     physical_activity: float
     diet_score: float
     glucose: float
-    hb1ac: float
+    hba1c: float
     medical_condition: str
 
 # 4. Define prediction endpoint
 @app.post("/predict")
 def predict_hospital_stay(data: PatientData):
     # Convert input data to numpy array
-    X_input = np.array([[
-        data.gender,
-        data.age,
-        data.alcohol,
-        data.smoking,
-        data.bmi,
-        data.physical_activity,
-        data.diet_score,
-        data.glucose,
-        data.hb1ac,
-        data.medical_condition
-    ]])
-    #Make prediction
-    prediction = model.predict(X_input)[0]
+    X_input = pd.DataFrame([{
+    "gender": data.gender,
+    "age": data.age,
+    "alcohol": data.alcohol,
+    "smoking": data.smoking,
+    "bmi": data.bmi,
+    "physical_activity": data.physical_activity,
+    "diet_score": data.diet_score,
+    "glucose": data.glucose,
+    "hba1c": data.hba1c,
+    "medical_condition": data.medical_condition
+}])
+    # Prétraitement (identique à l’entraînement)
+    X_processed = preprocessor.transform(X_input)
 
-    return {"predicted_hospital_stay_days": round(prediction)}
+    # Prédiction
+    prediction = model.predict(X_processed)[0]
+
+    return {"predicted_hospital_stay_days": round(float(prediction), 2)}
 
 # 5. Root endpoint
 @app.get("/")
 def root():
     return {"message": "Bienvenue à l'API de prédiction de la durée d'hospitalisation!"}    
+
